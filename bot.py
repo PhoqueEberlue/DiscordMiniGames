6c4b9f4b48ce10cfa8b75557759186a4f6b2226f
Andrew <58080347+PhoqueEberlue@users.noreply.github.com>
2020-04-20 00:58:47 +0200

    Update TODO

--- bot.py
+++ bot.py
@@ -218,6 +218,36 @@ async def play(ctx: commands.Context):
 
 
 
+###################################################################### POWER 4 ######################################################################
+# @bot.command()
+# async def power4(ctx: commands.Context):
+#     await ctx.send("Party created, click on the reaction bellow to join!")
+#     partyMessage = ctx.channel.last_message
+#     await partyMessage.add_reaction("✅")
+
+# @bot.command()
+# async def playPower4(ctx: commands.Context):
+#     reac = None
+#     players = []
+#     async for message in ctx.channel.history(limit=100):
+#         if message.author == bot.user:
+#             reac = message.reactions[0]
+#             break
+#     try:
+#         players.append(reac.users()[1])
+#         players.append(reac.users()[2])
+#         await ctx.send(players)
+#     except:
+#         await ctx.send("frero y'a pas de joueurs qu'est-ce que tu fait là ?")
+#     await ctx.send(f'joueur 1 : {players[0].mention} joueur 2 : {players[1].mention}')
+#     end = False
+#     while(not end):
+#         pass
+################################################################### ENDOF POWER 4 ###################################################################
+
+
+
+
 
 
 bot.run(getToken())
