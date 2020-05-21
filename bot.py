bfffcc7e6bac93de2093c5dbf605b2734b546560
Andrew <58080347+PhoqueEberlue@users.noreply.github.com>
2020-05-21 19:18:39 +0200

    I made commands that allows me to unload cogs in particular. So now i don't have to shut down and restart the bot if i'm working on a cog.

--- bot.py
+++ bot.py
@@ -8,18 +8,29 @@ bot = commands.Bot(command_prefix='$')
 async def on_ready():
     print('bot is ready')
 
-# @bot.command()
-# async def load(ctx: commands.Context, extension):
-#     bot.load_extension(f'cogs.{extension}')
+@bot.command()
+async def load(ctx: commands.Context, extension):
+    if ctx.author.id == 205434999888019456:
+        try:
+            bot.load_extension(f'games.{extension}.Cog{extension}')
+            await ctx.send(f'Le module {extension} à été load')
+        except ModuleNotFoundError as error:
+            await ctx.send(error)
 
-# @bot.command()
-# async def unload(ctx: commands.Context, extension):
-#     bot.unload_extension(f'cogs.{extension}')
+@bot.command()
+async def unload(ctx: commands.Context, extension):
+    if ctx.author.id == 205434999888019456:
+        try:
+            bot.unload_extension(f'games.{extension}.Cog{extension}')
+            await ctx.send(f'Le module {extension} à été unload')
+        except commands.errors.ExtensionNotLoaded as error:
+            await ctx.send(error)
+    
 
 #Loads up every cogs in the ./games file
 for rep in os.listdir('./games'):
     for file_name in os.listdir('./games/' + rep):
         if file_name.startswith('Cog') and file_name.endswith('.py'):
-            bot.load_extension('games.' + rep + f'.{file_name[:-3]}') #Deletes the ".py" at the end of the filename
+            bot.load_extension(f'games.{rep}.{file_name[:-3]}') #Deletes the ".py" at the end of the filename
 
 bot.run(getToken())
